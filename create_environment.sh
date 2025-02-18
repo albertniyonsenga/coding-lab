9b05b4bdf236813e0508ccb5d979091b295fd4f7
albertniyonsenga <a.niyonseng@alustudent.com>
2025-02-18 12:53:46 -0800

    Adding the main core part of our project structure

--- create_environment.sh
+++ create_environment.sh
@@ -1,2 +1,13 @@
-#!/bin/bash
-# Here we go 
+#!/usr/bin/env bash
+# Here we go, first we clarify the `shebang line` to work on every machine
+
+# Getting user's input to rename the submission reminder app repository
+read -p "Enter your name: " user_name
+
+# Creating the main repo, plus other sub-repositories
+main_repo="submission_reminder_$user_name"
+
+mkdir -p "$main_repo/config" "$main_repo/scripts" "$main_repo/data"
+
+# Let's go to the content of the files in their respective sub-repository
+
